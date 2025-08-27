781431fa79e373c67ee66a4d0244863a13bd508d
Allison Codi <allison.codi@emory.edu>
2025-08-27 16:06:40 -0400

    rerun 3 inflation

--- simulations/run_simulation.sh
+++ simulations/run_simulation.sh
@@ -9,7 +9,7 @@ module load R/4.4.0
 
 export PATH=/apps/R/4.4.0/bin:/usr/local/bin:/usr/bin:/usr/local/sbin
 
-# note set cpus-per-task at 6 sample sizes * 2 inflation * 2 effect protect = 24
+# note set cpus-per-task at 5 sample sizes * 3 inflation * 2 effect protect = 30
 
 # * iterate over 1000 seeds in array
 
@@ -19,7 +19,7 @@ sbatch --array=1-$NSEEDS \
   --partition=$PARTITION \
   --nodes=1 \
   --ntasks-per-node=1 \
-  --cpus-per-task=24 \
+  --cpus-per-task=30 \
   --mem-per-cpu=6G \
   --job-name=vegrowth_sim \
   --output=/projects/dbenkes/allison/vegrowth_analysis/scratch/${SETTING}_%A_%a.out \
