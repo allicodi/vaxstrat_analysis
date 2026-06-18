58e1bada8896b7d7daf3bdbc21fa856788536130
Allison Codi <allison.codi@emory.edu>
2026-06-18 16:51:23 -0400

    file name

--- simulations/run_simulation_cross_fit.sh
+++ simulations/run_simulation_cross_fit.sh
@@ -24,4 +24,4 @@ sbatch --array=1-$NSEEDS \
   --job-name=cross_fit \
   --output=/projects/dbenkes/allison/vegrowth_analysis/scratch/${SETTING}_%A_%a.out \
   --export=SETTING=$SETTING,NSEEDS=$NSEEDS \
-  --wrap "/apps/R/4.4.0/bin/Rscript R/run_simulation_cross_fit.R"
+  --wrap "/apps/R/4.4.0/bin/Rscript R/run_cross_fit_simulation.R"
